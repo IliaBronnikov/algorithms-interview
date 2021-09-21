9a8229ade9953b425933cd2992a650d70cd7158e
Ilia <iliabronn@gmail.com>
2021-09-22 02:04:18 +0300

    STRING Added valid palindrom(test task) solution + updated solution md builder(NEED rewrite with double open md file for read and write)

--- solution_md_builder.py
+++ solution_md_builder.py
@@ -1,24 +1,46 @@
-TASK_LINK = 'https://leetcode.com/problems/valid-palindrome/'
-TASK_TITLE = 'Valid Palindrome'
-TASK_SOLUTION = '''    def isPalindrome(self, s: str) -> bool:
-        r = re.sub('[^a-zA-Z0-9]', '', s)
-        if len(r) == 1:
-            return True
-        r = r.lower()
-        for i in range(len(r)//2):
-            if r[i] != r[-i-1]:
-                return False
-        return True    '''
-SOLUTION_MD_TEMPLATE = '+ [{}](#{})\n\n## {}\n\n{}\n\n```python\n{}\n```\n'
+import sys
+import argparse
+
+def createParser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('name_task_block')
+    parser.add_argument('code', type=open)
+    parser.add_argument('name_task',nargs='+')
+    return parser
+
+
 
 
 def build_code_block(solution):
-    return '\n'.join([line[4:] for line in solution.split('\n')])
+    return ''.join([line for line in solution])
 
 
 if __name__ == '__main__':
-    link_suffix = TASK_LINK.split("/")[-2]
-    result_md = SOLUTION_MD_TEMPLATE\
-        .format(TASK_TITLE, link_suffix, TASK_TITLE, TASK_LINK, build_code_block(TASK_SOLUTION))
-    print(result_md)
+    parser = createParser()
+    namespace = parser.parse_args(sys.argv[1:])
+
+    TASK_LINK = 'https://leetcode.com/problems/' + '-'.join(namespace.name_task).lower() + '/'
+    TASK_TITLE = ' '.join(namespace.name_task)
+
+    with open('text_solution.txt') as f:
+        TASK_SOLUTION = f.readlines()
+    f.close()
+
+    with open(namespace.name_task_block.lower() +'.md', mode = 'r+') as s:
+        s.readline()
+        s.readline()
+        full = s.readlines()
+        link_suffix = TASK_LINK.split("/")[-2]
+        full.insert(full.index('\n'), '+ [{}](#{})\n'.format(TASK_TITLE, link_suffix))
+        full.append('\n\n## {}\n\n{}\n\n```python\n{}\n```\n'\
+        .format(TASK_TITLE, TASK_LINK, build_code_block(TASK_SOLUTION)))
+    s.close()
+
+    with open(namespace.name_task_block.lower() +'.md', mode = 'w') as p:
+        p.write('#' + namespace.name_task_block.title() +'\n\n')
+        for line in full:
+            p.write(line)
+    p.close()
+
+
 
