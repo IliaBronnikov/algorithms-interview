8368771d8cdaaecb26dfbbdc3b68aa59ca2e62f4
Ilia <iliabronn@gmail.com>
2021-09-22 22:28:40 +0300

    Updated solution_md_builder.py file. Divided all commands on func.

--- solution_md_builder.py
+++ solution_md_builder.py
@@ -11,32 +11,41 @@ def createParser():
 def build_code_block(solution):
     return ''.join([line for line in solution])
 
-if __name__ == '__main__':
-    parser = createParser()
-    namespace = parser.parse_args(sys.argv[1:])
-
-    TASK_LINK = 'https://leetcode.com/problems/' + '-'.join(namespace.name_task).lower() + '/'
-    TASK_TITLE = ' '.join(namespace.name_task)
-
-    with open('text_solution.txt') as f:
-        TASK_SOLUTION = f.readlines()
-    f.close()
-
-    with open(namespace.name_task_block.lower() +'.md', mode = 'r+') as s:
+def collect_data(task_block):
+    with open(task_block.lower() +'.md', mode = 'r') as s:
         s.readline()
         s.readline()
         full = s.readlines()
-        link_suffix = TASK_LINK.split("/")[-2]
-        full.insert(full.index('\n'), '+ [{}](#{})\n'.format(TASK_TITLE, link_suffix))
-        full.append('\n\n## {}\n\n{}\n\n```python\n{}\n```\n'\
-        .format(TASK_TITLE, TASK_LINK, build_code_block(TASK_SOLUTION)))
     s.close()
+    return full
 
-    with open(namespace.name_task_block.lower() +'.md', mode = 'w') as p:
-        p.write('#' + namespace.name_task_block.title() +'\n\n')
-        for line in full:
+def update_md_file(list_full_text):
+        with open('text_solution.txt') as f:
+            TASK_SOLUTION = f.readlines()
+        f.close()
+        TASK_LINK = 'https://leetcode.com/problems/' + '-'.join(namespace.name_task).lower() + '/'
+        TASK_TITLE = ' '.join(namespace.name_task)
+
+        link_suffix = TASK_LINK.split("/")[-2]
+        list_full_text.insert(list_full_text.index('\n'), '+ [{}](#{})\n'.format(TASK_TITLE, link_suffix))
+        list_full_text.append('\n\n## {}\n\n{}\n\n```python\n{}\n```\n'\
+        .format(TASK_TITLE, TASK_LINK, build_code_block(TASK_SOLUTION)))
+
+def record_new_md_file(task_block, list_full_text):
+    with open(task_block.lower() +'.md', mode = 'w') as p:
+        p.write('#' + task_block.title() +'\n\n')
+        for line in list_full_text:
             p.write(line)
     p.close()
+
+
+if __name__ == '__main__':
+    parser = createParser()
+    namespace = parser.parse_args(sys.argv[1:])
+    full_text = collect_data(namespace.name_task_block)
+    update_md_file(full_text)
+    record_new_md_file(namespace.name_task_block, full_text)
+
 
 
 
