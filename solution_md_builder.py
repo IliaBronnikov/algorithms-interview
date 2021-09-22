b646686ded4355e24bfc7779798d889cbfcd512a
Ilia <iliabronn@gmail.com>
2021-09-22 23:02:32 +0300

    Updated solution_md_builder.py file. Changed read_data and update_md_file func

--- solution_md_builder.py
+++ solution_md_builder.py
@@ -11,21 +11,23 @@ def createParser():
 def build_code_block(solution):
     return ''.join([line for line in solution])
 
-def collect_data(task_block):
+def read_data(task_block):
     with open(task_block.lower() +'.md', mode = 'r') as s:
         s.readline()
         s.readline()
         full = s.readlines()
-    s.close()
-    return full
 
-def update_md_file(list_full_text):
-        with open('text_solution.txt') as f:
-            TASK_SOLUTION = f.readlines()
-        f.close()
+    with open('text_solution.txt') as f:
+        TASK_SOLUTION = f.readlines()
+
+    s.close()
+    f.close()
+
+    return full, TASK_SOLUTION
+
+def update_md_file(list_full_text, TASK_SOLUTION):
         TASK_LINK = 'https://leetcode.com/problems/' + '-'.join(namespace.name_task).lower() + '/'
         TASK_TITLE = ' '.join(namespace.name_task)
-
         link_suffix = TASK_LINK.split("/")[-2]
         list_full_text.insert(list_full_text.index('\n'), '+ [{}](#{})\n'.format(TASK_TITLE, link_suffix))
         list_full_text.append('\n\n## {}\n\n{}\n\n```python\n{}\n```\n'\
@@ -42,8 +44,8 @@ def record_new_md_file(task_block, list_full_text):
 if __name__ == '__main__':
     parser = createParser()
     namespace = parser.parse_args(sys.argv[1:])
-    full_text = collect_data(namespace.name_task_block)
-    update_md_file(full_text)
+    full_text, TASK_SOLUTION = read_data(namespace.name_task_block)
+    update_md_file(full_text, TASK_SOLUTION)
     record_new_md_file(namespace.name_task_block, full_text)
 
 
