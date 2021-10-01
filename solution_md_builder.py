249fba592829c29d41bd01fafe4802f2473538a7
Ilia <iliabronn@gmail.com>
2021-10-02 00:30:22 +0300

    Updated solution_md_builder.py file. Added test file

--- solution_md_builder.py
+++ solution_md_builder.py
@@ -1,5 +1,7 @@
 import sys
 import argparse
+import ast
+comment = '<!--  -->'
 
 def createParser():
     parser = argparse.ArgumentParser()
@@ -8,29 +10,30 @@ def createParser():
     parser.add_argument('name_task',nargs='+')
     return parser
 
-def build_code_block(solution):
-    return ''.join([line for line in solution])
-
 def read_data(text_file):
+    data = ''
     with open(text_file) as s:
-        list_name = s.readlines()
+        data = s.read()
     s.close()
-    return list_name
+    return data
 
 def build_constr_get_leetcode_sol(name_task, text_solution):
-    NT = ' '.join(name_task)
-    NT_L = '-'.join(name_task).lower()
-    return {'+ [{}](#{})\n'.format(NT, NT_L):'\n\n## ' + NT + '\n\nhttps://leetcode.com/problems/' + NT_L +
-                                            '\n\n```python\n' + text_solution + '\n```'}
+    NameTask = ' '.join(name_task)
+    NameTask_L = '-'.join(name_task).lower()
+    link = 'https://leetcode.com/problems/'
+    key_text = '+ [{}](#{})\n'.format(NameTask, NameTask_L)
+    value_text = '\n\n## {}\n\n{}{}\n\n```python\n{}\n```'.format(NameTask, link, NameTask_L, text_solution)
+    return {key_text:value_text}
+
 
 def get_splitted_md(old_md_file):
-    return {old_md_file.split('<!--  -->')[0]:old_md_file.split('<!--  -->')[1]}
+    return {old_md_file.split(comment)[0]:old_md_file.split(comment)[1]}
 
 def get_full_md(old_md_file, new_md_file):
-    return list(old_md_file.keys())[0] + list(new_md_file.keys())[0] + '<!--  -->' + list(old_md_file.values())[0] + \
+    return list(old_md_file.keys())[0] + list(new_md_file.keys())[0] + comment + list(old_md_file.values())[0] + \
            list(new_md_file.values())[0]
 
-def record_data(name_file, full_text):
+def write_data(name_file, full_text):
     with open(name_file, mode = 'w') as p:
         for line in full_text:
             p.write(line)
@@ -41,15 +44,13 @@ if __name__ == '__main__':
     namespace = parser.parse_args(sys.argv[1:])
 
     old_md = read_data(namespace.name_task_block.lower() + '.md')
-    old_md = build_code_block(old_md)
-    TASK_SOLUTION_LIST = read_data('text_solution.txt')
-    TASK_SOLUTION_STR = build_code_block(TASK_SOLUTION_LIST)
+    TASK_SOLUTION = read_data('text_solution.txt')
 
-    prep_sol = build_constr_get_leetcode_sol(namespace.name_task, TASK_SOLUTION_STR)
+    prep_sol = build_constr_get_leetcode_sol(namespace.name_task, TASK_SOLUTION)
     old_md_split = get_splitted_md(old_md)
     new_md = get_full_md(old_md_split, prep_sol)
 
-    record_data(namespace.name_task_block.lower() +'.md', new_md)
+    write_data(namespace.name_task_block.lower() +'.md', new_md)
 
 
 
---
+++ test_solution_md_builder.py
@@ -0,0 +1,22 @@
+from solution_md_builder import *
+import unittest
+
+class TestBuilder(unittest.TestCase):
+    def test_build_constr_get_leetcode_sol_1(self):
+        self.assertCountEqual(build_constr_get_leetcode_sol(['Valid', 'Palindrome'], 'I am the best'),
+                               {'+ [{}](#{})\n'.format('Valid Palindrome', 'valid-palindrome'):'\n\n## ' + 'Valid Palindrome' +
+                                                                  '\n\nhttps://leetcode.com/problems/'  +
+                                                                                'valid-palindrome' +
+                                            '\n\n```python\n' + 'I am the best' + '\n```'})
+
+    def test_get_splitted_md_1(self):
+        self.assertCountEqual(get_splitted_md('+ [Va Pal](#va-pal)\n<!--  -->\n## Va Pal'),{'+ [Va Pal]('
+                                                                                            '#va-pal)\n': '## Va Pal'})
+    def test_get_splitted_md_2(self):
+        self.assertCountEqual(get_splitted_md('abc<!--  -->cba'),{'abc':'cba'})
+
+    def test_get_full_md_1(self):
+        self.assertCountEqual(get_full_md({'1':'2'},{'3':'4'}), '13<!--  -->24')
+
+if __name__ == "__main__":
+    unittest.main()
