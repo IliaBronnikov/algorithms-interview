cb7bf6cf14d9e87060a7e1e1f7df5aec2541ea62
Ilia <iliabronn@gmail.com>
2021-10-03 23:50:52 +0300

    Updated test_solution_md_builder.py file. Rewrite import from solution_md_builder.py

--- test_solution_md_builder.py
+++ test_solution_md_builder.py
@@ -1,9 +1,9 @@
-from solution_md_builder import *
+import solution_md_builder as smb
 import unittest
 
 class TestBuilder(unittest.TestCase):
     def test_build_constr_get_leetcode_sol(self):
-        self.assertCountEqual(build_constr_get_leetcode_sol(['Valid', 'Palindrome'], 'I am the best'),
+        self.assertCountEqual(smb.build_constr_get_leetcode_sol(['Valid', 'Palindrome'], 'I am the best'),
                                {'new_md_link':'+ [Valid Palindrome](#valid-palindrome)\n', 'new_code_block': '\n\n## '
                                                                                                              'Valid '
                                                                              'Palindrome\n'
@@ -13,14 +13,14 @@ class TestBuilder(unittest.TestCase):
 
 
     def test_get_splitted_md(self):
-        self.assertCountEqual(get_splitted_md('+ [Va Pal](#va-pal)\n<!--  -->\n## Va Pal'),{'old_md_link':'+ [Va Pal]('
+        self.assertCountEqual(smb.get_splitted_md('+ [Va Pal](#va-pal)\n<!--  -->\n## Va Pal'),{'old_md_link':'+ [Va Pal]('
                                                                                             '#va-pal)\n',
                                                                                             'old_code_block': '## Va '
                                                                                                               'Pal'})
 
 
     def test_get_full_md(self):
-        self.assertCountEqual(get_full_md({'old_md_link':'1', 'old_code_block':'2'},{'new_md_link':'3',
+        self.assertCountEqual(smb.get_full_md({'old_md_link':'1', 'old_code_block':'2'},{'new_md_link':'3',
                                                                                      'new_code_block':'4'}),
         '13<!--  -->24')
 
