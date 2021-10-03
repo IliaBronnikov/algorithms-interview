7153291a2b81c82a1eec332a98101d5d4172cf29
Ilia <iliabronn@gmail.com>
2021-10-03 23:41:42 +0300

    Debug solution_md_builder.py file, rewrite and solved a fuck of a lot of notes.

--- solution_md_builder.py
+++ solution_md_builder.py
@@ -1,6 +1,7 @@
 import sys
 import argparse
-DIVIDED_CONSTANT = '<!--  -->'
+import re
+MD_FILE_DELIMITER = '<!--  -->'
 
 
 def createParser():
@@ -19,30 +20,32 @@ def read_data(text_file):
     return data
 
 
-def build_constr_get_leetcode_sol(name_task, text_solution):
+def get_link_from_text(text_data):
+    return re.search(r'https://.*problems/', text_data).group(0)
+
+
+def build_constr_get_leetcode_sol(name_task, text_solution, link):
     name_task_str = ' '.join(name_task)
     name_task_low_str = '-'.join(name_task).lower()
-    link = 'https://leetcode.com/problems/'
     key_text = '+ [{}](#{})\n'.format(name_task_str, name_task_low_str)
-    value_text = '\n\n## {}\n\n{}{}\n\n```python\n{}\n```'.format(name_task_str, link, name_task_low_str, text_solution)
-    return {'new_md_link':key_text, 'new_code_block':value_text}
+    value_text = '\n\n## {}\n\n{}{}/\n\n```python\n{}\n```'.format(name_task_str, link, name_task_low_str,
+                                                                   text_solution)
+    return {'md_link':key_text, 'code_block':value_text}
 
 
 def get_splitted_md(old_md_file):
-    return {'old_md_link': old_md_file.split(DIVIDED_CONSTANT)[0],'old_code_block' :old_md_file.split(
-                                                                  DIVIDED_CONSTANT)[1]}
+    return {'md_link': old_md_file.split(MD_FILE_DELIMITER)[0],'code_block' :old_md_file.split(
+                                                                  MD_FILE_DELIMITER)[1]}
 
 
 def get_full_md(old_md_file, new_md_file):
-    return '{}{}{}{}{}'.format(old_md_file['old_md_link'], new_md_file['new_md_link'], DIVIDED_CONSTANT, old_md_file[
-        'old_code_block'], new_md_file['new_code_block'])
-
+    return '{}{}{}{}{}'.format(old_md_file['md_link'], new_md_file['md_link'], MD_FILE_DELIMITER, old_md_file[
+        'code_block'], new_md_file['code_block'])
 
 
 def write_data(name_file, full_text):
     with open(name_file, mode = 'w') as p:
-        for line in full_text:
-            p.write(line)
+        p.write(full_text)
     p.close()
 
 if __name__ == '__main__':
@@ -50,11 +53,12 @@ if __name__ == '__main__':
     namespace = parser.parse_args(sys.argv[1:])
 
     old_md = read_data(namespace.name_task_block.lower() + '.md')
-    TASK_SOLUTION = read_data('text_solution.txt')
+    task_solution = read_data('text_solution.txt')
+    link = get_link_from_text(old_md)
 
-    prep_sol = build_constr_get_leetcode_sol(namespace.name_task, TASK_SOLUTION)
+    prepared_solution = build_constr_get_leetcode_sol(namespace.name_task, task_solution, link)
     old_md_split = get_splitted_md(old_md)
-    new_md = get_full_md(old_md_split, prep_sol)
+    new_md = get_full_md(old_md_split, prepared_solution)
 
     write_data(namespace.name_task_block.lower() +'.md', new_md)
 
--- test_solution_md_builder.py
+++ test_solution_md_builder.py
@@ -2,7 +2,7 @@ from solution_md_builder import *
 import unittest
 
 class TestBuilder(unittest.TestCase):
-    def test_build_constr_get_leetcode_sol_1(self):
+    def test_build_constr_get_leetcode_sol(self):
         self.assertCountEqual(build_constr_get_leetcode_sol(['Valid', 'Palindrome'], 'I am the best'),
                                {'new_md_link':'+ [Valid Palindrome](#valid-palindrome)\n', 'new_code_block': '\n\n## '
                                                                                                              'Valid '
@@ -11,15 +11,15 @@ class TestBuilder(unittest.TestCase):
                                                                              'valid-palindrome\n\n```python\nI am '
                                                                              'the best\n```'})
 
-    def test_get_splitted_md_1(self):
+
+    def test_get_splitted_md(self):
         self.assertCountEqual(get_splitted_md('+ [Va Pal](#va-pal)\n<!--  -->\n## Va Pal'),{'old_md_link':'+ [Va Pal]('
                                                                                             '#va-pal)\n',
                                                                                             'old_code_block': '## Va '
                                                                                                               'Pal'})
-    def test_get_splitted_md_2(self):
-        self.assertCountEqual(get_splitted_md('abc<!--  -->cba'),{'old_md_link' : 'abc','old_code_block':'cba'})
 
-    def test_get_full_md_1(self):
+
+    def test_get_full_md(self):
         self.assertCountEqual(get_full_md({'old_md_link':'1', 'old_code_block':'2'},{'new_md_link':'3',
                                                                                      'new_code_block':'4'}),
         '13<!--  -->24')
