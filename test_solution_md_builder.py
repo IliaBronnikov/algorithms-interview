d8a94159df59ea4a6e56917346f0baccf1570926
Ilia <iliabronn@gmail.com>
2021-10-05 23:48:36 +0300

    ta sha final

--- test_solution_md_builder.py
+++ test_solution_md_builder.py
@@ -1,14 +1,13 @@
 import solution_md_builder as smb
 import unittest
-IN_STR = 'Test name\nhttps://leetcode.com/\nclass\n    1stline\n    2ndline'
-OUT_STR = '+ [Test name](#test-name)\n<!--  -->2\n\n## Test name\n\nhttps://leetcode.com/test-name/\n\n' \
-                  '```python\n1stline\n2ndline\n```'
+
 
 class TestBuilder(unittest.TestCase):
     def test_build_constr_get_leetcode_sol(self):
+        in_str = 'Test name\nhttps://leetcode.com/\nclass\n    1stline\n    2ndline'
         out_dict = {'md_link':'+ [Test name](#test-name)\n',
             'code_block': '\n\n## Test name\n\nhttps://leetcode.com/test-name/\n\n```python\n1stline\n2ndline\n```'}
-        self.assertCountEqual(smb.build_constr_get_leetcode_sol(IN_STR), out_dict)
+        self.assertCountEqual(smb.build_constr_get_leetcode_sol(in_str), out_dict)
 
 
     def test_get_splitted_md(self):
@@ -23,15 +22,17 @@ class TestBuilder(unittest.TestCase):
 
 
     def test_content_new_md_file_notempty_md(self):
+        in_str = 'Test name\nhttps://leetcode.com/\nclass\n    1stline\n    2ndline'
         out_str = '+ [Test name](#test-name)\n<!--  -->2\n\n## Test name\n\nhttps://leetcode.com/test-name/\n\n' \
                   '```python\n1stline\n2ndline\n```'
-        self.assertCountEqual(smb.get_content_new_md_file(IN_STR, '1<!--  -->2', 'string.md'), '1{}'.format(out_str))
+        self.assertCountEqual(smb.get_content_new_md_file(in_str, '1<!--  -->2', 'string.md'), '1{}'.format(out_str))
 
 
     def test_content_new_md_file_empty_md(self):
+        in_str = 'Test name\nhttps://leetcode.com/\nclass\n    1stline\n    2ndline'
         out_str = '+ [Test name](#test-name)\n<!--  -->\n\n## Test name\n\nhttps://leetcode.com/test-name/\n\n' \
                   '```python\n1stline\n2ndline\n```'
-        self.assertCountEqual(smb.get_content_new_md_file(IN_STR, '', 'string.md'), '# String\n\n{}'.format(out_str))
+        self.assertCountEqual(smb.get_content_new_md_file(in_str, '', 'string.md'), '# String\n\n{}'.format(out_str))
 
 if __name__ == "__main__":
     unittest.main()
